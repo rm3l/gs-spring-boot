6f255a63eadd1098cb0c0a6197a304f36e78b046
Armel Soro <asoro@redhat.com>
2022-07-18 17:34:34 +0200

    play with odo add binding

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,10 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+        //My comment
+        System.out.println(String.format("[stdout 12] %s", new java.util.Date()));
+        System.err.println(String.format("[stderr 12] %s", new java.util.Date()));
+		return "12- Greetings from Spring Boot!!!";
 	}
 
 }
